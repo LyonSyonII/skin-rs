ad13a1d70f83ef4ee9ac134d0ef0998da805ba86
Liam G <liam.garriga@tutanota.com>
2022-07-05 17:55:56 +0200

    Added file size and type

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
 mod structs;
-use structs::Cli;
-use clap::Parser;
 
-fn main() {
-    let cli = Cli::parse();
-    println!("{cli:?}")
+use std::fs::FileType;
+
+use colored::Colorize;
+use structs::Cli;
+
+fn file_type_to_human(ftype: FileType) -> &'static str {
+    if ftype.is_dir() {
+        "Directory"
+    } else if ftype.is_file() {
+        "File"
+    } else if ftype.is_symlink() {
+        "Symlink"
+    } else {
+        "Could not determine file type"
+    }
+}
+
+
+fn bytes_to_human(bytes: u64) -> String {
+    const GiB: u64 = 1024*1024*1024;
+    const MiB: u64 = 1024*1024;
+    const KiB: u64 = 1024;
+
+    if bytes > GiB {
+        format!("{:.2}GiB", bytes as f64/GiB as f64)
+    } else if bytes > MiB {
+        format!("{:.2}MiB", bytes as f32/MiB as f32)
+    } else if bytes > KiB {
+        format!("{:.2}KiB", bytes as f32/KiB as f32)
+    } else {
+        format!("{bytes}B")
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let cli = Cli::read()?;
+    let files = cli.files;
+    println!("{files:?}");
+    
+    for file in files {
+        let metadata = std::fs::metadata(&file)?;
+        println!("{}", file.file_name().unwrap().to_string_lossy().bright_red().bold());
+        println!("Type: {}", file_type_to_human(metadata.file_type()));
+        println!("Size: {}", bytes_to_human(metadata.len()))
+    }
+    
+    
+
+    Ok(())
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,4 +1,7 @@
-use std::path::PathBuf;
+use std::{path::{PathBuf}, io::{Read}};
+
+use clap::Parser;
+use colored::Colorize;
 
 #[derive(clap::Parser, Debug)]
 #[clap(
@@ -7,16 +10,44 @@ use std::path::PathBuf;
     version,
 )]
 pub struct Cli {
+    #[clap(long)]
+    pub debug: bool,
     
     
-    
-    
-    #[clap(required=true)]
-    files: Vec<PathBuf>,
+    #[clap()]
+    pub files: Vec<PathBuf>,
 }
 
 impl Cli {
-    //pub fn read() -> Vec<PathBuf> {
-    //    let cli
-    //}
+    pub fn read() -> std::io::Result<Cli> {
+        let mut cli = Cli::parse();
+        let mut stdin = std::io::stdin().lock();
+        let mut pipe = String::new();
+        
+        if cli.debug {
+            dbg!(atty::is(atty::Stream::Stdin));
+            dbg!(atty::is(atty::Stream::Stdout));
+            dbg!(atty::is(atty::Stream::Stderr));
+        }
+        
+        // If something is piped in, read it and append
+        if atty::isnt(atty::Stream::Stdin) {
+            stdin.read_to_string(&mut pipe)?;
+        }
+
+        let pipe = pipe.split_ascii_whitespace().map(PathBuf::from);
+        cli.files.extend(pipe);
+        cli.files.sort();
+        cli.files.dedup();
+
+        if cli.files.is_empty() {
+            println!("{} The following required arguments were not provided:", "error:".bright_red().bold());
+            println!("    {}", "<FILES>...\n".green());
+            println!("USAGE:\n    skin [OPTIONS] <FILES>...\n");
+            println!("For more information try {}", "--help".green());
+            std::process::exit(1)
+        }
+        
+        Ok(cli)
+    }
 }
